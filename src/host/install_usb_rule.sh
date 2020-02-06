5c1faf9e562632a0eec4519f5e356d96d64f0bea
snopf <snopf@snopf.com>
2020-02-06 20:27:42 +0100

    explicitly check if user is root for usb rule install

--- src/host/install_usb_rule.sh
+++ src/host/install_usb_rule.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
+fi
+
 echo "Copying 52-snopf.rules to /etc/udev.rules.d"
 cp 52-snopf.rules /etc/udev/rules.d
 echo "Reloading rules"
