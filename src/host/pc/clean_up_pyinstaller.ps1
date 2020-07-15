8d01a5b60a5fddbd60fdcbec194cb53b28dd2e29
snopf <snopf@snopf.com>
2020-07-15 13:59:16 +0200

    change to onefile option for pyinstaller

--- src/host/pc/clean_up_pyinstaller.ps1
+++
@@ -1,8 +0,0 @@
- #!/bin/bash
- 
-$dist_path=".\..\dist\pc\snopf\"
- 
-Remove-Item -Path ${dist_path}libcrypto-1_1.dll
-Remove-Item -Path ${dist_path}Qt5Pdf.dll
-Remove-Item -Path ${dist_path}Qt5Quick.dll
-Remove-Item -Path ${dist_path}Qt5VirtualKeyboard.dll
