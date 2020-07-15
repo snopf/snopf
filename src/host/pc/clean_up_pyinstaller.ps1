9ff2b93c78edd3453de7e94a09f8c1c2d441c04b
snopf <snopf@snopf.com>
2020-07-15 13:38:28 +0200

    fix pyinstaller cleanup for windows

--- src/host/pc/clean_up_pyinstaller.ps1
+++ src/host/pc/clean_up_pyinstaller.ps1
@@ -1,14 +1,8 @@
  #!/bin/bash
  
-$dist_path="..\dist\pc\snopf\"
+$dist_path=".\..\dist\pc\snopf\"
  
-Remove-Item -Path ${dist_path}libQt5WebEngineCore.so.5 -ErrorAction Ignore
-Remove-Item -Path ${dist_path}libQt53DRender.so.5 -ErrorAction Ignore
-Remove-Item -Path ${dist_path}libcrypto.so.1.1 -ErrorAction Ignore
-Remove-Item -Path ${dist_path}libQt5Charts.so.5 -ErrorAction Ignore
-Remove-Item -Path ${dist_path}libQt5Bluetooth.so.5 -ErrorAction Ignore
-Remove-Item -Path ${dist_path}libQt5Multimedia.so.5 -ErrorAction Ignore
-Remove-Item -Path ${dist_path}libQt5Quick3DRuntimeRender.so.5 -ErrorAction Ignore
-Remove-Item -Path ${dist_path}PySide2/QtQml.abi3.so -ErrorAction Ignore
-Remove-Item -Path ${dist_path}PySide2/qml -Recurse -ErrorAction Ignore
-Remove-Item -Path ${dist_path}libQt5Pdf.so -ErrorAction Ignore
+Remove-Item -Path ${dist_path}libcrypto-1_1.dll
+Remove-Item -Path ${dist_path}Qt5Pdf.dll
+Remove-Item -Path ${dist_path}Qt5Quick.dll
+Remove-Item -Path ${dist_path}Qt5VirtualKeyboard.dll
